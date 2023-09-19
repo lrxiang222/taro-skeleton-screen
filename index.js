f16ae2af682e4ccbeac1a60fd9e3c1e0f8f29417
liangjingxiang <liangruxiang@ruiec.cn>
2023-09-19 11:28:46 +0800

    修改配置

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
  * @Author: kime
  * @Date: 2023-09-19 10:40:59
  * @LastEditors: kime
- * @LastEditTime: 2023-09-19 11:19:00
+ * @LastEditTime: 2023-09-19 11:28:32
  * @Description: 
  */
 console.log("hello world");
 console.log("验证GithubAction的发布流程");
 console.log("打一个Tag标签");
+console.log("修改工作流配置");
