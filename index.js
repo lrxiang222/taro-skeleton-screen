652113f48e36d24372638c650eac8fc476abfa3d
liangjingxiang <liangruxiang@ruiec.cn>
2023-09-19 11:20:29 +0800

    origin_ssh/dev

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @Author: kime
  * @Date: 2023-09-19 10:40:59
  * @LastEditors: kime
- * @LastEditTime: 2023-09-19 10:58:31
+ * @LastEditTime: 2023-09-19 11:19:00
  * @Description: 
  */
 console.log("hello world");
 console.log("验证GithubAction的发布流程");
+console.log("打一个Tag标签");
