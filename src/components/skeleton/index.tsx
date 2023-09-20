413fcb3fd295927c632d3326a09639a8c1dec42f
liangjingxiang <liangruxiang@ruiec.cn>
2023-09-20 17:59:33 +0800

    增加rollup进行模块化打包

---
+++ src/components/skeleton/index.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: kime
+ * @Date: 2023-09-20 15:41:10
+ * @LastEditors: kime
+ * @LastEditTime: 2023-09-20 17:55:27
+ * @Description: 
+ */
+import Taro from '@tarojs/taro'
+import { View, Block } from '@tarojs/components'
+
+export default function Skeleton () {
+    return (<View>Hello World</View>)
+}
